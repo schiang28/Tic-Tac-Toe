e54de64df01d6428aaa277113e493ca17a05f6ad
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-04-27 15:55:59 +0100

    game

--- Game.py
+++ Game.py
@@ -1,19 +1,29 @@
+from regex import R
+
+
 class Game:
+
+    EMPTY = " "
+    P1 = "0"
+    P2 = "X"
+
     def __init__(self):
-        pass
+        self.__board = [[Game.EMPTY for _ in range(3)] for _ in range(3)]
+        self.__player = Game.P1
 
     def __repr__(self):
-        pass
+        return str(self.__board)
 
     def play(self, row, col):
-        pass
+        row -= 1
+        col -= 1
+        self.__board[row][col] = self.__player
+        self.__player = Game.P2 if self.__player == Game.P1 else Game.P1
 
     @property
     def winner(self):
-        pass
+        return None
 
 
 if __name__ == "__main__":
     pass
-
-# comment
